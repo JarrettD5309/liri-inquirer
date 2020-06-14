b8cdfea18404f2c349527ab342d285835f4553f9
Jarrett Dougherty <jarrettdougherty@gmail.com>
2020-06-14 01:35:00 -0400

    setup more packages. started working on inquirer

--- liri.js
+++ liri.js
@@ -5,7 +5,26 @@ var Spotify = require('node-spotify-api');
 
 var moment = require('moment');
 
+var axios = require('axios');
+
+var fs = require("fs");
+
+var inquirer = require('inquirer');
+
 var spotify = new Spotify({
     id: process.env.SPOTIFY_ID,
     secret: process.env.SPOTIFY_SECRET
-  });
+});
+
+inquirer
+    .prompt([
+        {
+            type: "list",
+            message: "Which would you like to do?",
+            choices: ["Search for a song on Spotify", "Search for a film on OMDB", "Search for an artist on Bands in Town", "Do What It Says"],
+            name: "searchOption"
+        }
+    ])
+    .then(function (inquirerResponse) {
+        console.log(inquirerResponse.searchOption);
+    });
